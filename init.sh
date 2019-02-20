f96815504fbb0a2a6985155842adda75ab56c67d
Kevin Edwards <kedwards@kevinedwards.ca>
2019-02-20 03:26:49 -0700

    added ubuntu and debian configurations.

--- init.sh
+++ init.sh
@@ -2,9 +2,12 @@
 
 apt-get update 
 
-apt-get install git make python-setuptools gcc python-dev libffi-dev libssl-dev python-packaging
+apt-get install git make python-setuptools gcc python-dev libffi-dev libssl-dev python-packaging software-properties-common
 
-if [ ! -f /etc/apt/sources.list.d/ansible.list ]
+if [ $(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}') == 'ubuntu' ]
+then
+    apt-add-repository --yes --update ppa:ansible/ansible
+elif [ ! -f /etc/apt/sources.list.d/ansible.list ]
 then
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | tee /etc/apt/sources.list.d/ansible.list
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
@@ -12,3 +15,4 @@ fi
 
 apt-get update
 apt-get install ansible
+
