44fe5ae1c3ee172a228ce0de92cdada3bd566499
Kevin Edwards <kedwards@kevinedwards.ca>
2020-06-17 21:38:53 -0600

    updated terraform and vitualbox

--- install.sh
+++ install.sh
@@ -14,33 +14,26 @@ else
   SUDO_PASS=$1
 fi
 
+
 RELEASE=$(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}')
-ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main"
-ANSIBLE_APT_LIST=/etc/apt/sources.list.d/download_ansible_launchpad_net.list
-ANSIBLE_LIST=$(grep "${ANSIBLE_URL}" "${ANSIBLE_APT_LIST}")
+# CODENAME=$(lsb_release -cs)
+CODENAME=eoan
+ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/${RELEASE} ${CODENAME} main"
+
+#ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
+ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
 
 if [ "${RELEASE}" == 'ubuntu' ]
 then
-  apt-add-repository --yes --update ppa:ansible/ansible
+  echo "override due to focal release"
+  # apt-add-repository --yes --update ppa:ansible/ansible
 elif [ "${RELEASE}" == 'debian' ]
 then
   echo "${ANSIBLE_URL}" | tee "${ANSIBLE_APT_LIST}"
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  
-  aptget update && apt-get install -y git build-essential
-  
-  git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git 
-  
-  cp linux-firmware/iwlwifi-* /lib/firmware/
-  git clone https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/backport-iwlwifi.git
-  
-  cd backport-iwlwifi
-  make defconfig-iwlwifi-public
-  make -j4
-  make install
 fi
 
-apt-get update && apt-get install -y software-properties-common git ansible
+apt-get update && apt-get install -y ansible
 
 if [ ! -d /home/${SUDO_USER}/keca_sysinit ]
 then
