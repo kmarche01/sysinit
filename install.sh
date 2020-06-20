ed8ae03b85f544d623fa8e36d45973910dfe81d4
Kevin Edwards <kedwards@kevinedwards.ca>
2020-06-20 16:50:18 -0600

    updated to fix path issues

--- install.sh
+++ install.sh
@@ -14,23 +14,22 @@ else
   SUDO_PASS=$1
 fi
 
-
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 RELEASE=$(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}')
+
+# hardcode codename to release of focal and not in ppa yet
 # CODENAME=$(lsb_release -cs)
 CODENAME=eoan
-ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/${RELEASE} ${CODENAME} main"
 
-#ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
-ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
+ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/ubuntu ${CODENAME} main"
+ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${CODENAME}.list
 
 if [ "${RELEASE}" == 'ubuntu' ]
 then
-  echo "override due to focal release"
   # apt-add-repository --yes --update ppa:ansible/ansible
 elif [ "${RELEASE}" == 'debian' ]
 then
   echo "${ANSIBLE_URL}" | tee "${ANSIBLE_APT_LIST}"
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 fi
 
 apt-get update && apt-get install -y ansible
@@ -43,3 +42,27 @@ fi
 
 cd /home/${SUDO_USER}/keca_sysinit
 su -c "ansible-playbook playbook.yml -e 'ansible_sudo_pass=${SUDO_PASS}'" ${SUDO_USER}
+
+
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+RELEASE=$(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}')
+# CODENAME=$(lsb_release -cs)
+
+if [ "${RELEASE}" == 'ubuntu' ]
+then
+  CODENAME=eoan
+  ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/${RELEASE} ${CODENAME} main"
+  ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
+  # apt-add-repository --yes --update ppa:ansible/ansible
+elif [ "${RELEASE}" == 'debian' ]
+then
+  # CODENAME=$(awk -F"[=\"]+" '/VERSION_CODENAME=/ {print $2}' /etc/os-release)
+  CODENAME=eoan
+  ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/ubuntu ${CODENAME} main"
+  ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
+  echo "${ANSIBLE_URL}" | tee "${ANSIBLE_APT_LIST}"
+fi
+
+
+apt-get update && apt-get install -y ansible
