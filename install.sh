0b691ad9cc345b27f6577b8551818776ce3cd6d6
Kevin Edwards <kedwards@kevinedwards.ca>
2020-06-20 16:54:10 -0600

    clean up

--- install.sh
+++ install.sh
@@ -44,27 +44,3 @@ fi
 
 cd /home/${SUDO_USER}/keca_sysinit
 su -c "ansible-playbook playbook.yml -e 'ansible_sudo_pass=${SUDO_PASS}'" ${SUDO_USER}
-
-
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-RELEASE=$(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}')
-# CODENAME=$(lsb_release -cs)
-
-if [ "${RELEASE}" == 'ubuntu' ]
-then
-  CODENAME=eoan
-  ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/${RELEASE} ${CODENAME} main"
-  ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
-  # apt-add-repository --yes --update ppa:ansible/ansible
-elif [ "${RELEASE}" == 'debian' ]
-then
-  # CODENAME=$(awk -F"[=\"]+" '/VERSION_CODENAME=/ {print $2}' /etc/os-release)
-  CODENAME=eoan
-  ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/ubuntu ${CODENAME} main"
-  ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${RELEASE}-ansible-${CODENAME}.list
-  echo "${ANSIBLE_URL}" | tee "${ANSIBLE_APT_LIST}"
-fi
-
-
-apt-get update && apt-get install -y ansible
