7c2522a51cfbc41d6aec965b5b5522c415d0a32f
Kevin Edwards <kedwards@kevinedwards.ca>
2020-11-24 12:34:29 -0700

    updated for focal release

--- install.sh
+++ install.sh
@@ -20,30 +20,32 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 RELEASE=$(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}')
 
 # hardcode codename to release of focal and not in ppa yet
-# CODENAME=$(lsb_release -cs)
-CODENAME=eoan
+CODENAME=$(lsb_release -cs)
+# # CODENAME=eoan
 
 ANSIBLE_URL="deb http://ppa.launchpad.net/ansible/ansible/ubuntu ${CODENAME} main"
 ANSIBLE_APT_LIST=/etc/apt/sources.list.d/ansible-${CODENAME}.list
 
-if [ "${RELEASE}" == 'ubuntu' && $CODENAME === 'focal' ]
-then
-  # remove ppa
-  # apt-add-repository --yes --update ppa:ansible/ansible
-  echo "running"
-elif [ "${RELEASE}" == 'debian' ]
-then
-  echo "${ANSIBLE_URL}" | tee "${ANSIBLE_APT_LIST}"
-fi
+# if [ "${RELEASE}" == 'ubuntu' ] &&  [ $CODENAME === 'focal' ]
+# then
+#   # remove ppa
+#   # apt-add-repository --yes --update ppa:ansible/ansible
+#   echo "running"
+# elif [ "${RELEASE}" == 'debian' ]
+# then
+#   echo "${ANSIBLE_URL}" | tee "${ANSIBLE_APT_LIST}"
+# fi
 
 apt-get update && apt-get install -y ansible
 
 if [ ! -d ${SYSINIT_PATH} ]
 then
   git clone -b master --single-branch https://github.com/kedwards/sysinit.git ${SYSINIT_PATH}
-  cd ${SYSINIT_PATH} && git pull
 fi
+cd ${SYSINIT_PATH} && git pull
 
-cd ${SYSINIT_PATH}
-ansible-galaxy install geerlingguy.nodejs && ansible-galaxy collection install community.general
+# cd ${SYSINIT_PATH}
+
+# ansible-galaxy install geerlingguy.nodejs && ansible-galaxy collection install community.general
+ansible-galaxy collection install community.general
 su -c "ansible-playbook playbook.yml -e 'ansible_sudo_pass=${SUDO_PASS}'" ${SUDO_USER}
